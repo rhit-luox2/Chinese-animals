70bc3386cf4dd8904c7b341ad99bc8d34394e8b5
Kaila234234 <jamesk@rose-hulman.edu>
2024-04-02 10:32:01 -0400

    4/2/2024

---
+++ main/SelectAnimal.java
@@ -0,0 +1,26 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SelectAnimal {
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("12 Chinese Zodiac");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 900);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        Color backgroundColor = new Color(233, 197, 105);
+        panel.setBackground(backgroundColor);
+
+        JLabel title = new JLabel("12 Chinese Zodiac");
+        title.setFont(new Font("Arial", Font.BOLD, 24));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+}
