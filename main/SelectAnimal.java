35b5770715d32fe84dd8686bef2d27a4fb1c8c6a
rhit-luox2 <luox2@rose-hulman.edu>
2024-04-18 10:13:31 -0400

    images

--- main/SelectAnimal.java
+++ main/SelectAnimal.java
@@ -10,154 +10,46 @@ public class SelectAnimal extends JFrame {
     public SelectAnimal() {
         JFrame frame = new JFrame("Select Animal");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 900);
+        frame.setSize(900, 1200);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JPanel panel = new JPanel(new GridLayout(4, 3, 10, 10));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-        Color backgroundColor = new Color(233, 197, 105);
-        panel.setBackground(backgroundColor);
-
-        JLabel title = new JLabel("Please select an animal that you would like to know about");
-        title.setFont(new Font("Arial", Font.BOLD, 24));
-        title.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-        panel.add(Box.createVerticalStrut(20));
-
-        panel.add(title);
-        panel.add(Box.createRigidArea(new Dimension(0, 10)));
-
-        // 1
-        JButton rat = new JButton(new ImageIcon("picture/Rat.PNG"));
-        rat.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Rat();
-
-            }
-        });
-        rat.setBorder(BorderFactory.createEmptyBorder());
-        rat.setContentAreaFilled(false);
-        panel.add(rat);
-
-        // 2
-        JButton ox = new JButton("Ox");
-        ox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Ox();
-            }
-        });
-        panel.add(ox);
-
-        // 3
-        JButton tiger = new JButton("Tiger");
-        tiger.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Tiger();
-            }
-        });
-        panel.add(tiger);
-
-        // 4
-        JButton rabbit = new JButton("Rabbit");
-        rabbit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(frame, "You selected: Rabbit");
-            }
-        });
-        panel.add(rabbit);
-
-        // 5
-        JButton dragon = new JButton("Dragon");
-        dragon.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Dragon();
-            }
-        });
-        panel.add(dragon);
-
-        // 6
-        JButton horse = new JButton("Horse");
-        horse.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Horse();
-            }
-        });
-        panel.add(horse);
-
-        // 7
-        JButton snake = new JButton("Snake");
-        snake.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Snake();
-            }
-        });
-        panel.add(snake);
-
-        // 8
-        JButton goat = new JButton("Goat");
-        goat.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Goat();
-            }
-        });
-        panel.add(goat);
-
-        // 9
-        JButton monkey = new JButton("Monkey");
-        monkey.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Monkey();
-            }
-        });
-        panel.add(monkey);
-
-        // 10
-        JButton rooster = new JButton("Rooster");
-        rooster.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Rooster();
-            }
-        });
-        panel.add(rooster);
-
-        // 11
-        JButton dog = new JButton("Dog");
-        dog.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Dog();
-            }
-        });
-        panel.add(dog);
-
-        // 12
-        JButton pig = new JButton("Pig");
-        pig.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Pig();
-            }
-        });
-        panel.add(pig);
+        panel.setBackground(new Color(233, 197, 105));
 
         frame.add(panel);
+
+        // Array of animal names
+        String[] animals = { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Horse", "Snake", "Goat", "Monkey", "Rooster",
+                "Dog", "Pig" };
+        for (String animalName : animals) {
+            JButton animalButton = createAnimalButton(animalName);
+            panel.add(animalButton);
+        }
+
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
+    private JButton createAnimalButton(String animalName) {
+        ImageIcon icon = new ImageIcon("picture/" + animalName + ".PNG");
+        JButton animalButton = new JButton(icon);
+        animalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    Class.forName("main." + animalName).getDeclaredConstructor().newInstance();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        animalButton.setBorder(BorderFactory.createEmptyBorder());
+        animalButton.setContentAreaFilled(false);
+        animalButton.setPreferredSize(new Dimension(120, 120));
+        return animalButton;
+    }
+
     public static void main(String[] args) {
         new SelectAnimal();
-
     }
 }
