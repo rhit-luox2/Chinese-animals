4dcb53a4e0a721e33a75d15dfe44ca6b7767cf70
Kaila234234 <jamesk@rose-hulman.edu>
2024-04-20 22:16:15 -0400

    AnimalMain: created abstract class,

---
+++ main/AnimalMain.java
@@ -0,0 +1,13 @@
+package main;
+
+public abstract class AnimalMain {
+
+    String name;
+    String habitat;
+
+    public AnimalMain(String a, String b){
+        this.name = a;
+        this.habitat = b;
+    }
+
+}
