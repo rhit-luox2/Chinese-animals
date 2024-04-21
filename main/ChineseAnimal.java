082728e5500610cb7a8d30d58ede81cdc2633ef5
rhit-luox2 <luox2@rose-hulman.edu>
2024-04-21 15:20:07 -0400

    mianpage change

--- main/ChineseAnimal.java
+++ main/ChineseAnimal.java
@@ -31,61 +31,62 @@ public class ChineseAnimal {
 
         // Setting Title
         JLabel title = new JLabel("12 Chinese Zodiac");
-        title.setFont(new Font("Arial", Font.BOLD, 24));
+        title.setFont(new Font("Serif", Font.BOLD, 36));
+        title.setForeground(redColor);
         title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(title);
+
+        panel.add(Box.createVerticalStrut(30)); // Space between title and image
+
+        // Insert image
+        ImageIcon originalIcon = new ImageIcon("picture/main2.jpg");
+        Image image = originalIcon.getImage();
+        Image resizedImage = image.getScaledInstance(540, 540, Image.SCALE_SMOOTH);
+        ImageIcon resizedIcon = new ImageIcon(resizedImage);
+        JLabel imageLabel = new JLabel(resizedIcon);
+        imageLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(imageLabel);
 
         // Adds Text Box
-        panel.add(Box.createVerticalStrut(20));
+        panel.add(Box.createVerticalStrut(20)); // Space between image and text box
 
         JTextArea description = new JTextArea(
                 "The Lunar New Year has been celebrated for almost 3,500 years starting in the Shang Dynasty. " +
                         "Celebrations begin with the second new moon after the winter solstice. The Lunar New Year is heavily correlated with "
                         +
                         "Chinese zodiacs and each new year symbolizes the transition from one animal to the next.");
+        description.setFont(new Font("Serif", Font.ITALIC, 16));
         description.setWrapStyleWord(true);
         description.setLineWrap(true);
         description.setOpaque(false);
         description.setEditable(false);
         description.setAlignmentX(Component.CENTER_ALIGNMENT);
         description.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        panel.add(description);
 
         // Add Buttons to Lead to Other Pages
-        JButton learnMoreButton = new JButton("Click here to read about the Chinese zodiacs");
-        learnMoreButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        learnMoreButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new SelectAnimal();
-            }
-        });
-
-        // Wheel Page
-        JButton findyourZod = new JButton("Click here to find your zodiac or your friends");
-        findyourZod.setAlignmentX(Component.CENTER_ALIGNMENT);
-        findyourZod.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new WheelManager();
-            }
-            
-        });
-        {
-
-        }
-
-        // New Page
-
-        panel.add(title);
-        panel.add(Box.createRigidArea(new Dimension(0, 10)));
-        panel.add(description);
-        panel.add(Box.createRigidArea(new Dimension(0, 10)));
+        JButton learnMoreButton = createButton("Click here to read about the Chinese zodiacs", redColor);
         panel.add(learnMoreButton);
-        panel.add(Box.createRigidArea(new Dimension(0, 30)));
+
+        JButton findyourZod = createButton("Click here to find your zodiac or your friends", redColor);
         panel.add(findyourZod);
-        panel.add(Box.createRigidArea(new Dimension(0, 30)));
 
         frame.add(panel);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    private static JButton createButton(String text, Color textColor) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Arial", Font.BOLD, 14));
+        button.setForeground(textColor);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Placeholder for actual functionality
+            }
+        });
+        return button;
     }
 }
