b7fdd3955ba72523d98bfdfc41510636c6fe473d
Liz Purintun <purintea@rose-hulman.edu>
2024-05-08 03:58:25 -0400

    skd

--- main/game.java
+++ main/game.java
@@ -2,22 +2,37 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.HashMap;
+
 
 public class Game {
 
     private JFrame gameFrame;
-    private JPanel trackPanel;
+    protected JPanel trackPanel;
     private JLabel player1Label, player2Label;
+    private String player1, player2;
+
     private JButton startButton;
     private ImageIcon player1Icon, player2Icon;
     private int scorePlayer1 = 0, scorePlayer2 = 0;
+
     private boolean gameStarted = false;
     private final int winScore = 40;
 
-    protected Players players;
+    private Timer timer = new Timer(2000, null);
+    private Timer oneSecTimer;
+    private int counter;
+
+    private HashMap<JLabel, Integer> mpcRacers;
+    private HashMap<JLabel, Integer> mpcScores;
+
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Game()::launchGame);
@@ -54,43 +69,77 @@ public class Game {
         infoPanel.add(startButton);
         infoPanel.add(player2Label);
         gamePanel.add(infoPanel, BorderLayout.SOUTH);
+
         return gamePanel;
     }
 
     private void selectZodiacForPlayers() {
-        String[] animals = { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Horse", "Snake", "Goat", "Monkey", "Rooster",
-                "Dog", "Pig" };
-        player1Icon = selectZodiac("Player 1, choose your zodiac", animals);
+        ArrayList<String> animals = new ArrayList<String>(Arrays.asList( "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Horse", "Snake", "Goat", "Monkey", "Rooster",
+                "Dog", "Pig" ));
+        HashMap<ImageIcon, String> player1Pair = selectZodiac("Player 1, choose your zodiac", animals);
+        HashMap<ImageIcon, String> player2Pair = selectZodiac("Player 2, choose your zodiac", animals);
+        player1Icon = (ImageIcon)(player1Pair.keySet().toArray()[0]);
         player1Label.setIcon(new ImageIcon(player1Icon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
-        player2Icon = selectZodiac("Player 2, choose your zodiac", animals);
+        player2Icon = (ImageIcon)(player2Pair.keySet().toArray()[0]);
         player2Label.setIcon(new ImageIcon(player2Icon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
+
+        player1 = (String)(player1Pair.values().toArray()[0]);
+        player2 = (String)(player2Pair.values().toArray()[0]);
+        animals.remove(player1);
+        animals.remove(player2);
+        if (!animals.remove(player2)){
+            animals.remove(0);
+        }
+
+        mpcRacers = new HashMap<JLabel, Integer>();
+        mpcScores = new HashMap<JLabel, Integer>();
+        for (int i = 0; i < animals.size(); i++){
+            int speed = 2 + (int)Math.round(Math.random() * 3);
+            // I choose 2 as a minimul to reach the score in the time limit of 20 seconds
+            // and then at a maximum of 5 to reach score within 8 seconds. 
+            ImageIcon icon = new ImageIcon("picture/" + animals.get(i) + ".PNG");
+            JLabel otherLabel = new JLabel();
+            otherLabel.setIcon(new ImageIcon(icon.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH)));
+            mpcRacers.put(otherLabel, speed);
+            mpcScores.put(otherLabel,0);
+
+        }
+      
+        
+        
     }
 
-    private ImageIcon selectZodiac(String title, String[] animals) {
+    private HashMap<ImageIcon, String> selectZodiac(String title, ArrayList<String> animals) {
+
         JDialog dialog = new JDialog(gameFrame, title, true);
         dialog.setLayout(new GridLayout(0, 3));
         dialog.setSize(600, 600);
         dialog.setLocationRelativeTo(gameFrame);
-        ImageIcon[] icons = new ImageIcon[animals.length];
+        ImageIcon[] icons = new ImageIcon[animals.size()];
         AtomicReference<ImageIcon> selectedIcon = new AtomicReference<>();
-        for (int i = 0; i < animals.length; i++) {
-            ImageIcon icon = new ImageIcon("picture/" + animals[i] + ".PNG");
+        AtomicReference<String> selectedName = new AtomicReference<>();
+        for (int i = 0; i < animals.size(); i++) {
+            ImageIcon icon = new ImageIcon("picture/" + animals.get(i) + ".PNG");
             icons[i] = new ImageIcon(icon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
             final int index = i;
             JButton button = new JButton(icons[i]);
             button.addActionListener(e -> {
                 selectedIcon.set(icons[index]);
+                selectedName.set(animals.get(index));
                 dialog.dispose();
             });
             dialog.add(button);
         }
         dialog.setVisible(true);
-        return selectedIcon.get();
+        HashMap<ImageIcon,String> myMap = new HashMap<>();
+        myMap.put(selectedIcon.get(), selectedName.get());
+        return myMap;
     }
 
     private void startGame() {
         scorePlayer1 = 0;
         scorePlayer2 = 0;
+        timer.start();
         
         setupTrack();
         gameStarted = true;
@@ -115,6 +164,29 @@ public class Game {
         gameFrame.setFocusable(true);
         gameFrame.requestFocusInWindow();
         startButton.setEnabled(false);
+
+        counter = 0;
+        oneSecTimer = new Timer(1000, new ActionListener() {
+            @Override
+             public void actionPerformed(ActionEvent e) {
+                counter += 1;
+                for (JLabel labels: mpcRacers.keySet()){
+                    int newScore = mpcRacers.get(labels) + mpcScores.get(labels);
+                    mpcScores.replace(labels, newScore);
+                    moveIcon(labels, newScore);
+                    System.out.println(newScore);
+                }
+                if (counter < 20) {
+                    oneSecTimer.restart();
+                    System.out.println("heheehheheeh" + counter);
+                }
+                checkForWinner();
+
+             }
+
+        });
+        oneSecTimer.start();
+
     }
 
     private void moveIcon(JLabel label, int score) {
@@ -123,10 +195,17 @@ public class Game {
     }
 
     private void setupTrack() {
-        player1Label.setBounds(100, trackPanel.getHeight() - 50, 50, 50);
-        player2Label.setBounds(400, trackPanel.getHeight() - 50, 50, 50);
+        player1Label.setBounds(20, trackPanel.getHeight() - 50, 50, 50);
+        player2Label.setBounds(75, trackPanel.getHeight() - 50, 50, 50);
         trackPanel.add(player1Label);
         trackPanel.add(player2Label);
+        int xpos = 130;
+        for (JLabel labelToAdd : mpcRacers.keySet()){
+            trackPanel.add(labelToAdd);
+            labelToAdd.setLocation(xpos, trackPanel.getHeight() - 50);
+            xpos += 55;
+            
+        }
         trackPanel.revalidate();
         trackPanel.repaint();
     }
@@ -135,18 +214,40 @@ public class Game {
         if (scorePlayer1 >= winScore || scorePlayer2 >= winScore) {
             endGame();
         }
+        for (int score : mpcScores.values()) {
+            if (score >= winScore){
+                timer.stop();
+                oneSecTimer.stop();
+                endGame();
+            }
+        }
     }
 
     private void endGame() {
         gameStarted = false;
-        String winnerText = scorePlayer1 >= winScore ? "Player 1 wins!" : "Player 2 wins!";
+        String winnerText = "Computer Wins";
+        if (scorePlayer1 >= winScore){
+            winnerText = "Player 1 wins!";
+        }
+        else if (scorePlayer2 >= winScore){
+            winnerText = "Player 2 wins!";
+        }        
         JOptionPane.showMessageDialog(gameFrame, winnerText, "Game Over", JOptionPane.INFORMATION_MESSAGE);
         resetGame();
     }
 
     private void resetGame() {
-        player1Label.setLocation(100, trackPanel.getHeight() - 50);
-        player2Label.setLocation(400, trackPanel.getHeight() - 50);
+        player1Label.setLocation(20, trackPanel.getHeight() - 50);
+        player2Label.setLocation(75, trackPanel.getHeight() - 50);
+        int xpos = 130;
+        for (JLabel label : mpcRacers.keySet()){
+            mpcScores.replace(label, 0);
+            label.setLocation(xpos, trackPanel.getHeight() - 50);
+            xpos += 55;
+        }
+        scorePlayer1 = 0;
+        scorePlayer2 = 0;
+
         startButton.setEnabled(true);
         gameFrame.removeKeyListener(gameFrame.getKeyListeners()[0]);
     }
