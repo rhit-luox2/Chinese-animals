8b42a9e7b60f2a0ecc081cca1a4edf44522ed104
Kaila234234 <jamesk@rose-hulman.edu>
2024-05-18 15:12:30 -0400

    asda

--- main/Dog.java
+++ main/Dog.java
@@ -6,7 +6,8 @@ import javax.swing.*;
 public class Dog extends AnimalMain{
     
     public Dog(Language myLanguagee, String[] dogInfoArray) {
-        super(dogInfoArray[0],dogInfoArray[1],dogInfoArray[2], dogInfoArray[3], dogInfoArray[4], dogInfoArray[5], dogInfoArray[6], dogInfoArray[7], dogInfoArray[8], dogInfoArray[9], dogInfoArray[10], dogInfoArray[11], "picture/longdogimage.jpg", "picture/squaredDog.jpg", 
+        super(dogInfoArray[0],dogInfoArray[1],dogInfoArray[2], dogInfoArray[3], dogInfoArray[4], dogInfoArray[5], dogInfoArray[6], dogInfoArray[7], 
+        dogInfoArray[8], dogInfoArray[9], dogInfoArray[10], dogInfoArray[11], "picture/longdogimage.jpg", "picture/squaredDog.jpg", 
         myLanguagee);
         
         JFrame frame = new JFrame("Dog");
